4819895e2c0b2d309e7798a95c94ff8f753d2269
Alex Dredasty <uncommon.fresh@gmail.com>
2014-03-25 12:49:57 +0200

    Repository pattern implemented
    To increase the speed of getting values for repetitive descriptors for
    test, Repository pattern implemented based on data storage

--- src/main/java/org/grible/adaptor/DataStorage.java
+++ src/main/java/org/grible/adaptor/DataStorage.java
@@ -14,136 +14,145 @@ import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
- * Class that contains methods for retrieving data from grible Data Storages and transform it to descriptors objects.
+ * Class that contains methods for retrieving data from grible Data Storages and
+ * transform it to descriptors objects.
  * 
  * @author Maksym Barvinskyi
+ * @author Oleksii Pasko :)
  * 
  */
 public class DataStorage {
-
-	/**
-	 * Retrieves data from grible Data Storage and transforms it to descriptors objects.
-	 * 
-	 * @param type
-	 *            - class of the descriptor (i.e. UserInfo.class);
-	 * @return ArrayList of all the specified descriptors found in the storage.
-	 */
+	private static Map<Class<?>, TreeMap<Integer, ?>> descriptors = new HashMap<Class<?>, TreeMap<Integer, ?>>();
+	
+	@SuppressWarnings("unchecked")
 	public static <T> List<T> getDescriptors(Class<T> type) {
+		createDescriptorsEntryWithEmptyDescriptor(type);
+
+		TreeMap<Integer, T> map = (TreeMap<Integer, T>) descriptors.get(type);
+
 		TestTable dataStorage = new TestTable(type.getSimpleName());
-		List<T> result = new ArrayList<T>();
 		List<HashMap<String, String>> rows = dataStorage.getDataStorageValues();
+
 		for (int i = 0; i < rows.size(); i++) {
+			final int index = i + 1;
+			if (map.get(index) != null)	continue;
+			
 			T descriptor = null;
 			try {
 				Constructor<T> c = type.getConstructor(new Class[] { HashMap.class });
 				descriptor = c.newInstance(new Object[] { rows.get(i) });
 			} catch (Exception e) {
 				Exception gribleException = new Exception("DataStorage exception: " + e.getMessage()
-						+ ". Happened during creating a descriptor: " + type.toString() + " # " + (i + 1));
+						+ ". Happened during creating a descriptor: " + type.toString() + " # " + index);
 				gribleException.setStackTrace(e.getStackTrace());
 				GribleSettings.getErrorsHandler().onAdaptorFail(gribleException);
 			}
+			map.put(new Integer(index), descriptor);
+		}
+
+		return new ArrayList<T>(map.values());
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> List<T> getDescriptors(Class<T> type, Integer[] indexes) {
+		List<T> result = new ArrayList<T>();
+		createDescriptorsEntryWithEmptyDescriptor(type);
+
+		TreeMap<Integer, T> map = (TreeMap<Integer, T>) descriptors.get(type);
+
+		TestTable dataStorage = new TestTable(type.getSimpleName());
+		Map<Integer, HashMap<String, String>> rows = dataStorage.getDataStorageValues(indexes);
+
+		for (Integer index : indexes) {
+			final T value = map.get(index);
+			if (value != null) {
+				result.add(value);
+				continue;
+			}
+
+			T descriptor = null;
+			try {
+				Constructor<T> c = type.getConstructor(new Class[] { HashMap.class });
+				descriptor = c.newInstance(new Object[] { rows.get(index) });
+			} catch (Exception e) {
+				Exception gribleException = new Exception("DataStorage exception: " + e.getMessage()
+						+ ". Happened during creating a descriptor: " + type.toString() + " # " + index);
+				gribleException.setStackTrace(e.getStackTrace());
+				GribleSettings.getErrorsHandler().onAdaptorFail(gribleException);
+			}
+			map.put(new Integer(index), descriptor);
 			result.add(descriptor);
 		}
+
 		return result;
 	}
 
+	private static <T> void createDescriptorsEntryWithEmptyDescriptor(Class<T> type) {
+		if (descriptors.get(type) == null) {
+			TreeMap<Integer, T> map = new TreeMap<Integer, T>();
+			map.put(new Integer(0), createEmptyDescriptor(type));
+			descriptors.put(type, map);
+		}
+	}
+
 	/**
-	 * Retrieves data from grible Data Storage and transforms it to descriptors objects.
+	 * Retrieves data from grible Data Storage and transforms it to descriptors
+	 * objects.
 	 * 
 	 * @param type
 	 *            - class of the descriptor (i.e. UserInfo.class);
 	 * @param indexes
-	 *            - rows indexes of the descriptors to retrieve (i.e. "5", "1;2;2;7"); "0" index in multiple indexes
-	 *            ("1;0;7") is not allowed;
-	 * @return ArrayList of the descriptors with specified row numbers found in the storage.
+	 *            - rows indexes of the descriptors to retrieve (i.e. "5",
+	 *            "1;2;2;7");
+	 * @return ArrayList of the descriptors with specified row numbers found in
+	 *         the storage.
 	 */
 	public static <T> List<T> getDescriptors(Class<T> type, String indexes) {
-		return getDescriptors(type, indexes, false);
+		Integer[] iterationNumbers = getIntArrayFromString(indexes, ";");
+		return getDescriptors(type, iterationNumbers);
 	}
 
 	/**
-	 * Retrieves data from grible Data Storage and transforms it to descriptors objects.
-	 * 
-	 * @param type
-	 *            - class of the descriptor (i.e. UserInfo.class);
-	 * @param indexes
-	 *            - rows indexes of the descriptors to retrieve (i.e. "5", "1;2;2;7");
-	 * @param allowEmpty
-	 *            - specifies whether "0" index in multiple indexes (like "1;0;7") is allowed;
-	 * @return ArrayList of the descriptors with specified row numbers found in the storage.
-	 */
-	public static <T> List<T> getDescriptors(Class<T> type, String indexes, boolean allowEmpty) {
-		List<T> result = new ArrayList<T>();
-		if (allowEmpty) {
-			Integer[] iterationNumbers = { 0 };
-			if (indexes != null) {
-				iterationNumbers = getIntArrayFromString(indexes, ";");
-			}
-			TestTable dataStorage = new TestTable(type.getSimpleName());
-			HashMap<Integer, HashMap<String, String>> rows = dataStorage.getDataStorageValues(iterationNumbers);
-			for (int i = 0; i < iterationNumbers.length; i++) {
-				T descriptor = null;
-				try {
-					if (iterationNumbers[i] == 0) {
-						Constructor<T> c = type.getConstructor(new Class[] { HashMap.class });
-						descriptor = c.newInstance(new Object[] { null });
-					} else {
-						Constructor<T> c = type.getConstructor(new Class[] { HashMap.class });
-						descriptor = c.newInstance(new Object[] { rows.get(iterationNumbers[i]) });
-					}
-				} catch (Exception e) {
-					Exception gribleException = new Exception("DataStorage exception: " + e.getMessage()
-							+ ". Happened during creating a descriptor: " + type.toString() + " # "
-							+ iterationNumbers[i]);
-					gribleException.setStackTrace(e.getStackTrace());
-					GribleSettings.getErrorsHandler().onAdaptorFail(gribleException);
-				}
-				result.add(descriptor);
-			}
-		} else {
-			if ((!("0").equals(indexes)) && (indexes != null)) {
-				Integer[] iterationNumbers = getIntArrayFromString(indexes, ";");
-				TestTable dataStorage = new TestTable(type.getSimpleName());
-				HashMap<Integer, HashMap<String, String>> rows = dataStorage.getDataStorageValues(iterationNumbers);
-				for (int i = 0; i < iterationNumbers.length; i++) {
-					T descriptor = null;
-					try {
-						Constructor<T> c = type.getConstructor(new Class[] { HashMap.class });
-						descriptor = c.newInstance(new Object[] { rows.get(iterationNumbers[i]) });
-					} catch (Exception e) {
-						Exception gribleException = new Exception("DataStorage exception: " + e.getMessage()
-								+ ". Happened during creating a descriptor: " + type.toString() + " # "
-								+ iterationNumbers[i]);
-						gribleException.setStackTrace(e.getStackTrace());
-						GribleSettings.getErrorsHandler().onAdaptorFail(gribleException);
-					}
-					result.add(descriptor);
-				}
-			}
-		}
-		return result;
-	}
-
-	/**
-	 * Retrieves data from grible Data Storage and transforms it to the single descriptor object.
+	 * Retrieves data from grible Data Storage and transforms it to the single
+	 * descriptor object.
 	 * 
 	 * @param type
 	 *            - class of the descriptor (i.e. UserInfo.class);
 	 * @param index
-	 *            - row index of the descriptor to retrieve (i.e. "1", "5"); if "0", returns an empty descriptor;
-	 * @return Descriptor for specified row number found in the storage or an empty (which all properties are null) descriptor.
+	 *            - row index of the descriptor to retrieve (i.e. "1", "5"); if
+	 *            "0", returns an empty descriptor;
+	 * @return Descriptor for specified row number found in the storage or an
+	 *         empty (which all properties are null) descriptor.
 	 */
+	public static <T> T getDescriptor(Class<T> type, Integer index) {
+		return getDescriptors(type, String.valueOf(index)).get(0);
+	}
+
 	public static <T> T getDescriptor(Class<T> type, String index) {
-		List<T> descriptors = getDescriptors(type, index, true);
-		return descriptors.get(0);
+		return getDescriptors(type, index).get(0);
+	}
+
+	private static <T> T createEmptyDescriptor(Class<T> type) {
+		T descriptor = null;
+		try {
+			Constructor<T> c = type.getConstructor(new Class[] { HashMap.class });
+			descriptor = c.newInstance(new Object[] { null });
+		} catch (Exception e) {
+			Exception gribleException = new Exception("DataStorage exception: " + e.getMessage()
+					+ ". Happened during creating an empty descriptor: " + type.toString());
+			gribleException.setStackTrace(e.getStackTrace());
+			GribleSettings.getErrorsHandler().onAdaptorFail(gribleException);
+		}
+		return descriptor;
 	}
 
 	private static Integer[] getIntArrayFromString(String allElements, String delimiter) {
 		if (("").equals(allElements) || (allElements == null)) {
-			return new Integer[0];
+			return new Integer[]{0};
 		}
 		String[] tempArray = allElements.split(delimiter);
 		Integer[] resultArray = new Integer[tempArray.length];
